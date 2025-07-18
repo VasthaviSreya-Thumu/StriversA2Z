d702e68f6ef6d9065391156d746f5bb1061d289f
VasthaviSreya-Thumu <156116582+VasthaviSreya-Thumu@users.noreply.github.com>
2025-07-18 21:30:34 +0530

    Create code.py

---
+++ Basic Math/divisorsOfANumber/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def divisors(self, n):
+        res = []
+        for i in range(1, n + 1):
+            if n % i == 0:
+                res.append(i)
+        return res
+      
