981ceeeedd19383780097b46ab561e3a7e9950d6
VasthaviSreya-Thumu <156116582+VasthaviSreya-Thumu@users.noreply.github.com>
2025-11-11 22:04:39 +0530

    Implement selection sort algorithm in Java

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,19 @@
+  class Solution {
+    public int[] selectionSort(int[] nums) {
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i ++){
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[j] < nums[minIndex]){
+                    minIndex = j;
+                } 
+            }  
+            if (minIndex != i){
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+        return nums;
+    }
+}
