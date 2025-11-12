b14242fdb8656433912409c34ce597ffe0abf7b4
VasthaviSreya-Thumu <156116582+VasthaviSreya-Thumu@users.noreply.github.com>
2025-11-13 02:53:52 +0530

    Implement brute force method to move zeros to end

---
+++ Arrays/Logic Building/Move_Zeros_to_End_BruteForce.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++){
+            if (nums[i] != 0){
+                temp[count] = nums[i];
+                count++;
+            }
+        }
+        for (int i = count; i < n; i++){
+            temp[i] = 0; 
+        }
+        for (int i = 0; i < n; i++){
+            nums[i] = temp[i];
+        }
+    }
+}
